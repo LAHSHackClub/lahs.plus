0d4b24d901126696cc0c2190d4350e4348d067c3
AllusiveWheat <auraud.zarafshar@gmail.com>
2021-10-22 16:41:23 -0700

    NavBar & Darkmode Done

--- src/components/DarkToggle.svelte
+++ src/components/DarkToggle.svelte
@@ -1,4 +1,7 @@
 <script lang="ts" context="module">
+	import { onMount } from 'svelte';
+	let ready = false;
+
 	function darkToggle() {
 		if (localStorage.getItem('dark') == 'true') {
 			localStorage.setItem('dark', 'false');
@@ -6,16 +9,16 @@
 		}
 		if (localStorage.getItem('theme') === 'dark') {
 			localStorage.setItem('theme', 'light');
-			document.documentElement.setAttribute('data-theme', 'light');
 			document.body.classList.remove('dark');
 			document.body.classList.add('light');
 		} else {
 			localStorage.setItem('theme', 'dark');
-			document.documentElement.setAttribute('data-theme', 'dark');
 			document.body.classList.remove('light');
 			document.querySelector('body').classList.add('dark');
 		}
 	}
 </script>
+
+import {onMount} from 'svelte';
 
 <button class="navbar-link ml-auto " on:click={darkToggle}>Toggle Theme</button>
