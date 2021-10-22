f5b2e76113406d2753b40c0ea638a2895bb27326
Allusive <allusivewheat@gmail.com>
2021-10-21 18:24:52 -0700

    updated .gitignore for node_modules & package-lock.json

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to lahs.plus</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="css">
+h1 {
+  font-size: 2em;
+  margin: 0.67em 0;
+}
+p{
+    color: crimson;
+    padding: auto;
+    margin-top: auto;
+}
+p:hover{
+    color: blue;
+}
+</style>
