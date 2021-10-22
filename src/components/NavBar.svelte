87219db52b1187f6d360105d23e8a0f6bee88b34
AllusiveWheat <auraud.zarafshar@gmail.com>
2021-10-21 20:10:41 -0700

    Basic Navbar with routing. Needs styles

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,39 @@
+<script>
+	const NavItems = [
+		{
+			name: 'Home',
+			href: '/'
+		},
+		{
+			name: 'Map',
+			href: '/map'
+		}
+	];
+</script>
+
+<nav>
+	<div class="navbar">
+		{#each NavItems as item}
+			<li class="navbar-item">
+				<a class="navbar-link" href={item.href}> {item.name}</a>
+			</li>
+		{/each}
+	</div>
+</nav>
+
+<style>
+	.navbar {
+		background-color: antiquewhite;
+		overflow: hidden;
+	}
+	.navbar-item {
+		width: 100%;
+		justify-content: space-between;
+		margin: 0;
+		padding: 0 40px;
+	}
+	.navbar-link {
+		text-decoration: none;
+		color: black;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,23 @@
+<script>
+	import NavBar from '../components/NavBar.svelte';
+</script>
+
+<NavBar />
+
 <h1>Welcome to lahs.plus</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="css">
-h1 {
-  font-size: 2em;
-  margin: 0.67em 0;
-}
-p{
-    color: crimson;
-    padding: auto;
-    margin-top: auto;
-}
-p:hover{
-    color: blue;
-}
+	h1 {
+		font-size: 2em;
+		margin: 0.67em 0;
+	}
+	p {
+		color: crimson;
+		padding: auto;
+		margin-top: auto;
+	}
+	p:hover {
+		color: blue;
+	}
 </style>
