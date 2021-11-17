b75dcd6820ee8e70282b9c4adfa9803ab14603c4
Allusive <auraud.zarafshar@gmail.com>
2021-11-16 19:07:33 -0800

    This is very bad but going for a card design with each club in a grid. And uses a custom api to parse club info.

---
+++ src/components/Club.svelte
@@ -0,0 +1,51 @@
+<script>
+	export var key;
+	var club = 'hi';
+	var club_desc = '';
+</script>
+
+<div class="container">
+	<div class="inner">
+		<div class="club-title">
+			<h1>
+				Club: {club}
+			</h1>
+		</div>
+		<h3 class="club-desc">
+			{club_desc}
+		</h3>
+	</div>
+</div>
+
+<style>
+	.container {
+		width: 100%;
+		display: flex;
+		display: -webkit-flex;
+		justify-content: center;
+		-webkit-justify-content: center;
+		max-width: 820px;
+		border-top-left-radius: 12px;
+		border-top-right-radius: 12px;
+	}
+	.inner {
+		z-index: 2;
+		@apply bg-lahs-blue;
+		border-top-left-radius: 12px;
+		border-top-right-radius: 12px;
+		border-bottom-left-radius: 12px;
+		border-bottom-right-radius: 12px;
+		padding: 16px 24px 24px 24px;
+	}
+
+	.club-title {
+		font-size: 1.5em;
+		font-weight: bold;
+		margin-bottom: 0;
+	}
+	.club-desc {
+		font-size: 1em;
+		margin-top: 0;
+		word-wrap: break-word;
+	}
+</style>
--- src/routes/clubs.svelte
+++ src/routes/clubs.svelte
@@ -1,1 +1,21 @@
+<script>
+	import Club from '../components/Club.svelte';
+</script>
+
 <div>List of Clubs</div>
+
+<section class="club-section">
+	<Club key={1} />
+</section>
+
+<style>
+	.club-section {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		align-items: center;
+		margin: 0 auto;
+		width: 100%;
+		max-width: 1200px;
+	}
+</style>
