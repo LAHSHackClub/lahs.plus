80b52f8b6a136ef038c4d18508eb314f07bd5aff
domchang1 <dominicchang08@gmail.com>
2021-11-22 16:26:08 -0800

    new svg and better styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,9 +13,18 @@
 		<section class="flex flex-wrap w-1/3"> 
 			<section class="">
 				<div class ="flex flex-col bg-lahs-blue border-gray-300 rounded-lg shadow-lg p-6 text-white">
-					<details>
-						<summary class="flex justify-center align-middle text-3xl font-bold my-10 p-6 shadow-lg rounded-md">Attendance:</summary>
-						<div class="flex justify-around text-2xl font-semibold fill-current">
+					<details class="my-10">
+						<summary class="flex justify-center align-middle text-3xl font-bold p-6 shadow-lg rounded-md">
+							<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-calendar2-check" viewBox="0 0 16 16">
+								<path d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+								<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
+								<path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z"/>
+							</svg>
+							<span class="ml-3">Attendance:</span>
+							<!-- <svg><use xlink:href="/img/icons.svg#attendance"/></svg> -->
+							
+						</summary>
+						<div class="flex justify-around mt-10 text-2xl font-semibold fill-current">
 							<div class="dropdown">
 								<button class="dropbtn">
 									<svg class="stroke-width-2"><use xlink:href="/img/icons.svg#email"/></svg>
@@ -23,7 +32,7 @@
 								</button>
 								<div class="dropdown-info">
 									<a href="mailto:lahs.attendance@mvla.net" target="__blank"> 
-										<h6 class="phone">lahs.attendance@mvla.net</h6>
+										<h6 class="email text-base">lahs.attendance@mvla.net</h6>
 									</a>
 								</div>				
 							</div>
@@ -33,18 +42,18 @@
 									Phone
 								</button>
 								<div class="dropdown-info">
-									<h6 class="text-center">650-941-2761</h6>
+									<h6 class="text-center text-base phone">650-941-2761</h6>
 								</div>
 							</div>
 						</div>
 						<h2 class="flex justify-center text-2xl font-semibold mt-10 mb-10">Other Information:</h2>
-						<div class= "flex justify-center">
-							<ul class="text-1xl text-white">
+						<div class= "flex justify-center mb-10">
+							<ul class="text-base text-white">
 								<li>Parents must report an absence within 48 hours of an absence.</li>
 								<li>Due to technical difficulties with the phone system, they are temporarily accepting Attendance notifications from parents/guardians via email.
 									Please send your email to lahs.attendance@mvla.net.</li>
 								<li>The message must be sent from the parent/guardian's email account that is already on file with SIS student contact information, and include:
-									<ul class="text-5 ml-10 mt-2">
+									<ul class="text-5 mt-2">
 										<li>Student first and last name</li>
 										<li>Guardian name</li>
 										<li>Dates or class periods absent</li>
@@ -54,26 +63,38 @@
 							</ul>
 						</div>
 					</details>
-					<details class="info">
-						<summary class="flex justify-center align-middle text-3xl font-bold my-10 p-6 shadow-lg rounded-md">CHAC and Psychological Services:</summary>
-						<h3 class="flex justify-center text-xl mb-10 mt-20 font-semibold">Community Health Awareness Council (CHAC)</h3>
-						<div>
+					<details class="info my-10">
+						<summary class="flex justify-center align-middle text-3xl font-bold p-6 shadow-lg rounded-md">CHAC and Psychological Services:</summary>
+						<h3 class="flex justify-center text-2xl mb-10 mt-20 font-bold">Community Health Awareness Council (CHAC)</h3>
+						<div class="flex justify-around text-2xl font-semibold fill-current">
+							<div class="dropdown">
+								<button class="dropbtn">
+									<svg class="stroke-width-2"><use xlink:href="/img/icons.svg#email"/></svg>
+									Email
+								</button>
+								<div class="dropdown-info">
+									<a href="mailto:info@chacmv.org" target="__blank"> 
+										<h6 class="email text-base">info@chacmv.org</h6>
+									</a>
+								</div>				
+							</div>
+							<div class="dropdown">
+								<button class="dropbtn">
+									<svg class=""><use xlink:href="/img/icons.svg#phone"/></svg>
+									Phone
+								</button>
+								<div class="dropdown-info">
+									<h6 class="text-center text-base phone">650-965-2020</h6>
+								</div>
+							</div>
+						</div>
+						<div class="my-10 mx-5">
 							<h4>&emsp;&emsp;All of CHAC's programs are designed to protect young people from high-risk behaviors such as alcohol use,
 								illicit drug use, violence, and premature sexual activity, while building personal assets such as school success, valuing diversity, maintaining good health, and 
 								the ability to delay gratification; all of which will help insure that they have the greatest chance for success in life. 
 								A critical component of CHAC’s work is an “integrated care” model of working with four school districts to support the psychological and emotional well-being of 
 								students in K-12. All school programs are provided free of charge to students and their families. CHAC’s school-based on-site counseling services address social, 
 								emotional and mental health issues affecting students’ ability to learn. Staff consultation and training services enhance student support and success in the classroom.</h4>
-							<h3 class="text-xl ml-20 mb-4 mt-4 font-semibold">&emsp;&emsp;Other info?</h3>
-							<ul class="ml-24 text-base font-semibold">
-								<li>
-									Email:
-									<a href="mailto:info@chacmv.org" target="_blank">info@chacmv.org</a>
-								</li>
-								<li>
-									Phone: (650) 965-2020
-								</li>
-							</ul>
 						</div>
 					</details>
 				</div>
@@ -87,8 +108,9 @@
 
 <style lang="scss">
 
-	.info:hover{
-		outline: #FFFFFF;
+	details:hover{
+		background-color: #004FA9;
+		border-radius: 0.5 rem;
 	}
 
 	a:hover{
@@ -121,6 +143,11 @@
 		.dropbtn {
 			background: linear-gradient(side, #FFFFFF, lahs-blue);
 			white-space: normal;
+			transition: 1s;
+
+			&:hover .dropdown-info {
+			display: block;
+			}
 		}
 
 		.dropbtn svg {
@@ -128,14 +155,24 @@
 			width: 50px;
 		}
 
-		.phone{
+		.email{
 			max-width: 300px;
 			overflow-wrap: break-word;
 		}
 
-		&:hover .dropdown-info {
-			display: block;
-			transition: all 0.5s ease 0.5s;
+		.phone{
+			min-width: 120px;
+			
 		}
-	}	
+	}
+	.element {
+  			display: none;
+		}
+
+	@media (min-width: 768px) {
+  		.element {
+    	display: block;
+  		}
+	}
+	
 </style>
