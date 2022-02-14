c2fc674c7f8e985fb4081dcb0d2358133e60acca
Allusive <auraud.zarafshar@gmail.com>
2022-02-13 18:01:52 -0800

    Simple google embed untill we can do somthing better.

--- src/routes/calendar.svelte
+++ src/routes/calendar.svelte
@@ -1,1 +1,11 @@
 <div>School Calendar</div>
+
+<!-- Embeded google calender -->
+<iframe
+	src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=bXZsYS5uZXRfMzEzNjM2MzQzOTM1MzAzNjM2MzIzNkByZXNvdXJjZS5jYWxlbmRhci5nb29nbGUuY29t&color=%23B39DDB"
+	style="border:solid 1px #777"
+	width="800"
+	height="600"
+	frameborder="0"
+	scrolling="no"
+/>
